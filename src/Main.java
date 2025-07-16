bcf328c003448b2c8a1d1a0881164c12679ff1a7
Entesar-Qandil <entesarqandil211@gmail.com>
2025-07-16 14:17:26 +0300

    Add divide method (before pulling updates)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 2;
-
-        if (b != 0) {
-            int result = a / b;
-            System.out.println("Result of division: " + result);
-        } else {
-            System.out.println("Cannot divide by zero");
-        }
+       
     }
 }
