ff99e23e532af9af6f09d450dc291bc84584e315
Donia Assi <donia.assi@example.com>
2025-07-16 14:01:03 +0300

    Added mod method to Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+        public int mod(int a, int b) {
+            if (b == 0) {
+                throw new ArithmeticException("Cannot mod by zero");
+            }
+            return a % b;
+        }
+    }
+git add src/Main.java
