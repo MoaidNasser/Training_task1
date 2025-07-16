8a21c56a9df319399118b6180513408c0261b462
Moaid <moaidnasser19@gmail.com>
2025-07-16 13:53:17 -0700

    Define properties. And implement subtraction

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ class Calculator {
 
     private int a;
     private int b;
+
+
+    public int subtract(int a, int b) {
+        int result;
+
+        result = a - b;
+
+        return result;
+    }
+
 }
 
 
