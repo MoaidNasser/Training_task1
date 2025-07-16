3f6ddd8b1d55da88af05b2c6f35deccf8850c0ee
Entesar-Qandil <entesarqandil211@gmail.com>
2025-07-16 14:31:08 +0300

    Update main method with division finallllllllllllllllll

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-       
+
+
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
     }
 }
